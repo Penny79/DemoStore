acb80c2b5bec917bff875a98f918c8268cf795ad
Ralf Hoeppner <Ralf.Hoeppner@googlemail.com>
2013-02-20 16:55:34 +0100

    Implemented NHibernate Repo

---
+++ DemoStore.DataAccess/NHibernate/EFDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using DemoStore.Core.Entities;
+using DemoStore.DataAccess.Migrations;
+
+namespace DemoStore.DataAccess.EntityFramework
+{
+    /// <summary>
+    /// This class provides the access to a concrete database context. 
+    /// </summary>
+    public class EfDbContext : DbContext
+    {
+        public EfDbContext()
+        {
+            //set the initializer to migration
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<EfDbContext, Configuration>());
+            this.Database.Initialize(false);
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
